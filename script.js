71efd66ddf6dfbb73b05c10fb8b200b03b867af3
GENYF <gnyf0915@gmail.com>
2020-07-27 13:53:32 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function colorHandler(self, color){
+    var text = document.getElementById("yearText")
+    var box = document.getElementById("colorBox");
+    var value = self.value;
+
+    text.innerHTML = "Color of the Year " + value + " (" + color + ") ";
+    box.style.backgroundColor = color;
+}
